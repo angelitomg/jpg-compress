39d6a7507e16ec8706c613ef101a788fcda880fe
Angelito M. Goulart <angelito@polo-norte.(none)>
2013-10-03 07:56:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+jpg-compress
+====================
+Simple script to compress JPEG images
+
+Requeriments
+--------------------
+
+Commands required:
+* convert
+* cut
+* wc
+
+Usage
+--------------------
+
+Example with single file
+--------------------
+
+bash jpg-compress photo.jpg 80
+
+* photo.jpg -> file to compress (original image will be replaced by compressed image)
+* 80 -> image quality after compression
+
+Example with folder
+--------------------
+
+bash jpg-compress images/ 70
+* images -> folder with jpg images (original images will be replaced with compressed images)
+* 70 -> images quality after compression
+
+Author
+--------------------
+
+Angelito M. Goulart
+www.angelitomg.com
---
+++ jpg-compress.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+#############################
+# Compress jpg files
+#
+# Angelito M. Goulart
+# www.angelitomg.com 
+#
+#############################
+
+#############################
+# Help
+#############################
+if ([ -z "$1" ] || [ -z "$2" ]) || [ "$1" == "--help" ]
+then
+	echo -e "Usage: compress-jpg.sh /path 80
+
+Where path is the path of image or folder (for current directory use .)
+and 80 is quality of image after compression.
+
+WARNING: this script overwrites the original file!"
+	exit 0
+fi
+
+#############################
+# Function to compress jpg
+#############################
+function compress_jpg {
+	local orig_size="$(ls -l $1 | cut -d " " -f5)"
+	convert $1 -quality $2 $1
+	local new_size="$(ls -l $1 | cut -d " " -f5)"
+	total="$((total + (orig_size - new_size)))"
+	echo "$1 compressed $((orig_size / 2**10))K to $((new_size / 2**10))K"
+}
+
+#############################
+# Configs
+#############################
+total=0
+quality=$2
+path=$1
+
+
+##############################
+# Validate 'quality' parameter
+##############################
+re="^[0-9]+$"
+if ! [[ $quality =~ $re ]]
+then
+	echo "Quality must be a number"
+	exit 1
+fi
+
+######################
+# Convert single file
+######################
+if [ -f $path ]
+then
+	compress_jpg $path $quality
+else
+
+	#############################
+	# Convert all files of a dir
+	#############################
+	count="$(ls -1 $path/*.jpg 2> /dev/null | wc -l)"
+	if [ $count -lt 1 ]
+	then
+		echo "Directory $path has no jpg files."
+		exit 1
+	fi
+	for f in $path/*.jpg
+	do
+		compress_jpg $f $quality
+	done
+
+fi
+
+# Results
+echo "$(($total / 2**10))K saved"
